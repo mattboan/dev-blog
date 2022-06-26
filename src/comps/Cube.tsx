10c70fa7ce402cc47feae39fcfc8db1d8fa27bf8
mattboan <matt.n.boan@gmail.com>
2022-06-26 11:21:26 +0800

    Added basic cube comp

---
+++ src/comps/Cube.tsx
@@ -0,0 +1,35 @@
+import * as THREE from "three";
+import { useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber"
+
+
+// Basic three js cube component
+export const Cube = () => {
+    return <>
+        <CanvasWrapper />  
+    </>
+};  
+
+const CanvasWrapper = () => {
+    return <Canvas>
+        <ambientLight />
+        <CubeInner />
+    </Canvas>
+}
+
+const CubeInner = (props: JSX.IntrinsicElements['mesh']) => {
+    const ref = useRef<THREE.Mesh>(null!)
+    
+    useFrame((state, delta) => {
+        if (!ref?.current) return;
+        ref.current.rotation.x += 0.01 }
+    );
+
+    return (
+    <mesh
+    {...props}
+    ref={ref}>
+        <boxGeometry args={[1,1,1]} />
+        <meshStandardMaterial color={"orange"} />
+    </mesh>)
+}
--- src/comps/Hero.tsx
+++ src/comps/Hero.tsx
@@ -1,14 +1,15 @@
 import "../css/hero.css"
+import { Cube } from "./Cube";
 
 /**
  * Hero component
  */
 export const Hero = () => {
-    
     return <div className="major-con">
         <div className="hero-con">
             <div className="hero-img-temp" style={{ backgroundImage: "url('/img/3d-env.png')" }}>
             </div>
+            <Cube />
         </div>
     </div>
 };
