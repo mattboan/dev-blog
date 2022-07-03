af409c4bc8fa0b9da74f277a783265cbe0b75455
mattboan <matt.n.boan@gmail.com>
2022-07-03 10:54:24 +0800

    Footer to bottom, page to top

--- src/comps/Header.tsx
+++ src/comps/Header.tsx
@@ -1,9 +1,9 @@
-import "../css/header.css"
-import {faBars, faTimes} from "@fortawesome/free-solid-svg-icons";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import "../css/header.css";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect, useState } from "react";
 import { Menu } from "./Menu";
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 
 /**
  * Header component
@@ -11,34 +11,53 @@ import { motion } from "framer-motion"
 export const Header = () => {
     const [open, setOpen] = useState(false);
 
+    /** Watch the open variable */
     useEffect(() => {
-        document.body.style.overflow = open ? "hidden" : "unset"
+        if (open) {
+           // Scroll to the top TODO: instead of scrolling to the top of the page, move the header to the top of the body window
+           window.scrollTo(0, 0);
+        }
+
+        document.body.style.overflow = open ? "hidden" : "unset";
     }, [open]);
 
-    return <>
-    { open && <Menu open={() => setOpen(false) }/>}
-    <div className="header">
-        <div className="header-left">
-        <button className="menu-btn" onClick={() => !open &&setOpen(!open)} style={{
-            opacity: `${open ? "0%" : "100%"}`
-        }}>
-           <FontAwesomeIcon icon={faBars} />
-        </button>
-        <motion.div animate={open ? "open" : "closed"}variants={variants}>
-            <h1>mattboan</h1>
-        </motion.div>
-        </div>
+    return (
+        <>
+            {open && <Menu open={() => setOpen(false)} />}
+            <div className="header">
+                <div className="header-left">
+                    <button
+                        className="menu-btn"
+                        onClick={() => !open && setOpen(!open)}
+                        style={{
+                            opacity: `${open ? "0%" : "100%"}`,
+                        }}
+                    >
+                        <FontAwesomeIcon icon={faBars} />
+                    </button>
+                    <motion.div
+                        animate={open ? "open" : "closed"}
+                        variants={variants}
+                    >
+                        <h1>mattboan</h1>
+                    </motion.div>
+                </div>
 
-        <button className="menu-btn" onClick={() => open &&setOpen(!open)} style={{
-            opacity: `${open ? "100%" : "0%"}`
-        }}>
-           <FontAwesomeIcon icon={faTimes} />
-        </button>
-    </div>
-    </>
+                <button
+                    className="menu-btn"
+                    onClick={() => open && setOpen(!open)}
+                    style={{
+                        opacity: `${open ? "100%" : "0%"}`,
+                    }}
+                >
+                    <FontAwesomeIcon icon={faTimes} />
+                </button>
+            </div>
+        </>
+    );
 };
 
 const variants = {
     open: { x: "-40px" },
-    closed: {  x: 0 },
-}
+    closed: { x: 0 },
+};
